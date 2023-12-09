f62513954c0afd7eca70e485ea289d43732f697a
rafaelmings <rafaeld.785@hotmail.com>
2023-12-09 17:39:11 -0300

    Criação do botão para background personalizável

--- pages/feed.php
+++ pages/feed.php
@@ -9,12 +9,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/feed.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>Feed - PSG</title>
 </head>
 <body>
 
     <div class="navbar">
-        <h1>Welcome, <?php echo $_SESSION['username']; ?></h1>
+        <h1>Bem vindo(a), <?php echo $_SESSION['username']; ?></h1>
         <p>
             <a href="php/logout.php">Sair</a>
         </p>
@@ -30,12 +31,33 @@
                 <h2>Conteúdo Central</h2>
                 <p>Aqui será mostrado o feed...</p>
             </div>
+            <div class="img-button">
+                <input type="file" id="fileInput" accept="image/*" onchange="changeBackground(this)">
+                <label for="fileInput" id="uploadButton"><i class='bx bx-edit'></i></label>
+            </div>
         </div>
         <div class="sidebar" id="sidebar-direita">
             <h2>Aba Direita</h2>
             <p>Aqui será mostrado os amigos...</p>
         </div>
     </div>
+
+    <script>
+        function changeBackground(input) {
+            const file = input.files[0];
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function(e) {
+                    const contentDiv = document.getElementsByClassName('content')[0];
+                    contentDiv.style.backgroundImage = `url(${e.target.result})`;
+                };
+
+                reader.readAsDataURL(file);
+            }
+        }
+    </script>
 
 </body>
 </html>
